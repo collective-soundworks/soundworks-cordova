068144ead278b99ad54fcab5b5df5db42310889a
David Poirier-Quinot <davipoir@gmail.com>
2016-07-12 17:13:13 +0200

    removed document.addEventListener in beacon service, previously added to wait for service loading: the waiting was handled in client/index.js (wrongly so because of a typo).

--- src/client/player/index.js
+++ src/client/player/index.js
@@ -29,6 +29,6 @@ const init = () => {
 };
 
 if (!!window.cordova)
-  document.addEventListener('deviceready', init());
+  document.addEventListener('deviceready', init);
 else
-  window.addEventListener('load', init());
+  window.addEventListener('load', init);
